88d8ff4d25d72c02d920523a60b8c1d0ec9e63b6
MojtabaSr <87765388+MojtabaSr@users.noreply.github.com>
2022-03-06 18:41:58 +0100

    First push
    Första push

---
+++ pages/index.js
@@ -0,0 +1,75 @@
+import { Client } from "@notionhq/client";
+import { useEffect } from 'react';
+//const projectsId = cdae3ce226d44c21b810c95c6e86aa0c;
+//const peopleId = b7a24c0cba3f4582a6b24cd4548feeaa;
+//const timereportId = 8acace5aa128437da75c516327908aca;
+
+
+export default function Home({ results }) {
+
+    useEffect(() => {
+        console.log(results);
+    })
+
+
+    const getTidRapportColNames = () => {
+        const colNames = [];
+
+        /*for (var key in results[0].properties) {
+            console.log(key);
+            console.log(results[0].properties[key].id);
+        }*/
+
+
+
+        Object.keys(results[0].properties).forEach(function (key) {
+
+            colNames.push(
+                <th className="col" key={results[0].properties[key].id}>
+                    {key}
+                </th>
+            );
+
+        });
+        return colNames;
+    };
+
+    const getTidRapportEnteries = () => {
+        const enteries= [];
+
+
+
+        return enteries;
+    };
+    return (
+        <div>
+            <table>
+                <tbody>
+                    <tr>
+                        {getTidRapportColNames()}
+                    </tr>
+                    <tr>
+
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export async function getStaticProps() {
+    const notion = new Client({ auth: process.env.NOTION_API_KEY });
+    const databaseId = "cdae3ce226d44c21b810c95c6e86aa0c";
+    const response = await notion.databases.query({
+        database_id: databaseId,
+
+    });
+
+    //console.log(response);
+    return {
+        props: {
+            results: response.results,
+        },
+    };
+
+}
