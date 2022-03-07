c617abfa921a73186e7edbfe81f52c26e93c84f9
NoobPLPlayer1 <johnwalker0x62@gmail.com>
2022-03-07 14:37:25 +0100

    Cleanup

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import { Client } from "@notionhq/client";
 import { useEffect } from 'react';
 import Table from "./components/Table"; './components/Table.js'
+
 const projectsId = "cdae3ce226d44c21b810c95c6e86aa0c";
 const peopleId = "b7a24c0cba3f4582a6b24cd4548feeaa";
 const timereportId = "8acace5aa128437da75c516327908aca";
@@ -8,58 +9,9 @@ const timereportId = "8acace5aa128437da75c516327908aca";
 
 export default function Home({ results }) {
 
-    useEffect(() => {
-        //console.log(results);
-    })
-
-
-    const getTidRapportColNames = () => {
-        const colNames = [];
-
-        /*for (var key in results[0].properties) {
-            console.log(key);
-            console.log(results[0].properties[key].id);
-        }*/
-
-
-
-        Object.keys(results[0].properties).forEach(function (key) {
-
-            colNames.push(
-                <th className="col" key={results[0].properties[key].id}>
-                    {key}
-                </th>
-            );
-
-        });
-        return colNames;
-    };
-
-    const getTidRapportEnteries = () => {
-        const enteries= [];
-
-
-
-        return enteries;
-    };
     return (
         <div>
-            <Table 
-            fields={["Projectname", "Status", "Hours", "Worked hours", "Hours left", "Timespan"]} 
-            pages = {{}}
-            database = {results} />
-
-            {/*<table>
-                <tbody>
-                    <tr>
-                        {getTidRapportColNames()}
-                    </tr>
-                    <tr>
-
-                    </tr>
-                </tbody>
-            </table>
-            */}
+            <Table fields={["Projectname", "Status", "Hours", "Worked hours", "Hours left", "Timespan"]} database = {results} />
         </div>
     )
 }
@@ -70,18 +22,6 @@ export async function getStaticProps() {
     const response = await notion.databases.query({
         database_id: databaseId,
 
-    });
-
-    var pages = [];
-
-    var keys = Object.keys(response.results[0].properties);
-
-    keys.forEach((key) => {
-        var res = response.results[0].properties[key];
-        if(res.type == 'date'){
-            console.log(res);
-            pages.push(key)
-        }
     });
 
     //console.log(response);
