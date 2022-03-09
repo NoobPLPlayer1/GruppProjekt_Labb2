34227958bc29812e5c5db5149199e2f2653164e9
NoobPLPlayer1 <johnwalker0x62@gmail.com>
2022-03-09 15:02:43 +0100

    fix

--- pages/api/submit-form.js
+++ pages/api/submit-form.js
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 const timereportId = "8acace5aa128437da75c516327908aca";
 import { Client } from "@notionhq/client";
-import notion from '../misc/notion';
+const notion = new Client({ auth: process.env.NOTION_API_KEY });
 
 export default async function handler(req, res) {
 
