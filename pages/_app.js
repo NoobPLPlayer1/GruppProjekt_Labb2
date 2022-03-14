ef1cad0fab22e8215e41a4d475a838db29d12f02
NoobPLPlayer1 <johnwalker0x62@gmail.com>
2022-03-14 13:55:44 +0100

    Fake Login

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,25 @@
 import '../styles/globals.css'
 import Layout from './components/Layout'
 import Head from 'next/head'
+import {useState, useEffect} from 'react'
 
 function MyApp({ Component, pageProps }) {
+    const [currentUser, setCurrentUser] = useState("Signed out"); // Hämtade giltiga användare
+    useEffect( // Körs varje gång då antingen filter eller sortering ändras
+        () => {
+            console.log(currentUser);
+        }, [currentUser])
     return (
         <Layout>
-
             <Head>
                 <meta charSet='utf-8' />
                 <title>KoalaManager</title>
             </Head>
 
 
-            <Component {...pageProps} />
+            <Component {...pageProps} CurrentUser={currentUser} SetCurrentUser={setCurrentUser} />
 
-        </Layout>
-
-        
-        )
+        </Layout>)
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,7 @@
+import {useState, useEffect} from 'react'
+import { QueryDatabase } from './notion'
+import { Redirect } from "react-router-dom";
+import Link from 'next/link';
 import SearchTable from './components/SearchTable';
 import Form from "./components/Form"; './components/Form.js'
 import { notion } from './notion';
@@ -6,17 +10,52 @@ const peopleId = "b7a24c0cba3f4582a6b24cd4548feeaa";
 const timereportId = "8acace5aa128437da75c516327908aca";
 
 
-export default function Home({ results }) {
+export default function Home({ CurrentUser, SetCurrentUser, results }) {
+    return  <>{ (CurrentUser == "Signed out" ? <LoginHome SetCurrentUser={SetCurrentUser}/> : <ManageHome results={results} />) }</>
+}
 
-   
+function LoginHome({ CurrentUser, SetCurrentUser }){ // Login sidan
+    const [users, setUsers] = useState([]); // Hämtade giltiga användare
+
+    useEffect( // Körs när sidan laddas
+        () => {
+
+            async function GetData() {
+                // 'Promise.all()' väntar på att alla 'QueryDatabase's ska bli klar
+                await Promise.all([
+                    QueryDatabase("b7a24c0cba3f4582a6b24cd4548feeaa", undefined, undefined, setUsers)
+                ]);
+
+            }
+            GetData();
+        }, [])
+    
     return (
-        <div>
-            
-            <SearchTable fields={["Projectname", "Status", "Hours", "Worked hours", "Hours left", "Timespan"]} database={projectsId} />
-            <Form props={results} /> 
-            
-        </div>
-    )
+    <div>
+        <h2>Login</h2>
+        <form>
+            <label htmlFor="user">User </label>
+            <select
+                name="user"
+                id="user"
+                onChange={(e) => { SetCurrentUser(e.target.value); }}
+            >
+                <option value={"Signed out"}>
+                    Signed out
+                </option>
+                {users.map((user) => {
+                    return <option value={user.properties.Name.title[0].plain_text}>{user.properties.Name.title[0].plain_text}</option>
+                })}
+            </select>
+        </form>
+    </div>)
+}
+
+function ManageHome({ results }){ // Huvudsidan
+    return <div>
+        <SearchTable fields={["Projectname", "Status", "Hours", "Worked hours", "Hours left", "Timespan"]} database={projectsId} />
+        <Form props={results} /> 
+    </div>
 }
 
 export async function getServerSideProps() {
