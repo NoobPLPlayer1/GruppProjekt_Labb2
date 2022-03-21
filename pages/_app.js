1073bf496031d6a81c81be2a750fb73e64ccad03
NoobPLPlayer1 <johnwalker0x62@gmail.com>
2022-03-21 11:31:13 +0100

    login bug fix

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import '../styles/globals.css'
 import Layout from './components/Layout'
 import Head from 'next/head'
 import { useState, useEffect } from 'react'
+import { QueryDatabase } from './notion'
 
 
 function MyApp({ Component, pageProps }) {
@@ -9,17 +10,69 @@ function MyApp({ Component, pageProps }) {
     useEffect( // Körs varje gång då antingen användare ändras
         () => {
         }, [currentUser])
+    return (<>{
+        (currentUser == "Signed out" ? 
+            <Layout>
+                <Head>
+                    <meta charSet='utf-8' />
+                    <title>KoalaManager</title>
+                </Head>
+
+
+                <LoginHome CurrentUser={currentUser} SetCurrentUser={setCurrentUser} />
+
+            </Layout> :
+                <Layout>
+                <Head>
+                    <meta charSet='utf-8' />
+                    <title>KoalaManager</title>
+                </Head>
+
+
+                <Component {...pageProps} CurrentUser={currentUser} SetCurrentUser={setCurrentUser} />
+
+            </Layout>)} 
+            </>)
+}
+
+function LoginHome({ CurrentUser, SetCurrentUser }){ // Login sidan
+    const [users, setUsers] = useState([]); // Hämtade giltiga användare
+    
+    useEffect( // Körs när sidan laddas
+        () => {
+
+            async function GetData() {
+                // 'Promise.all()' väntar på att alla 'QueryDatabase's ska bli klar
+                await Promise.all([
+                    QueryDatabase("b7a24c0cba3f4582a6b24cd4548feeaa", undefined, undefined, setUsers)
+                ]);
+
+            }
+            GetData();
+        }, [])
+    
     return (
-        <Layout>
-            <Head>
-                <meta charSet='utf-8' />
-                <title>KoalaManager</title>
-            </Head>
-
-
-            <Component {...pageProps} CurrentUser={currentUser} SetCurrentUser={setCurrentUser} />
-
-        </Layout>)
+    <div>
+        <h2>Login</h2>
+        <form>
+            <label htmlFor="user">User </label>
+            <select
+                name="user"
+                id="user"
+                onChange={(e) => { SetCurrentUser(e.target.value); }}
+            >
+                <option value={"Signed out"}>
+                    Signed out
+                </option>
+                    {users.map((user) => {
+                        
+                        return <option key={user.id} value={user.id}>{user.properties.Name.title[0].plain_text}</option>
+                        
+                   
+                })}
+            </select>
+        </form>
+    </div>)
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,55 +1,14 @@
 import {useState, useEffect} from 'react'
-import { QueryDatabase } from './notion'
 import SearchTable from './components/SearchTable';
 import Timereport from "./components/Timereport"; './components/Timereport.js'
 import TimeChart from "./components/TimeChart"; './components/TimeChart.js'
 import { notion, projectsId } from './notion';
 
 export default function Home({ CurrentUser, SetCurrentUser, results }) {
-    return <>{
-        (CurrentUser == "Signed out" ? <LoginHome SetCurrentUser={SetCurrentUser} /> :
-            <ManageHome results={results} CurrentUser={CurrentUser} />)} </>
+    return <ManageHome results={results} CurrentUser={CurrentUser} />
 }
 
-function LoginHome({ CurrentUser, SetCurrentUser }){ // Login sidan
-    const [users, setUsers] = useState([]); // Hämtade giltiga användare
-    
-    useEffect( // Körs när sidan laddas
-        () => {
 
-            async function GetData() {
-                // 'Promise.all()' väntar på att alla 'QueryDatabase's ska bli klar
-                await Promise.all([
-                    QueryDatabase("b7a24c0cba3f4582a6b24cd4548feeaa", undefined, undefined, setUsers)
-                ]);
-
-            }
-            GetData();
-        }, [])
-    
-    return (
-    <div>
-        <h2>Login</h2>
-        <form>
-            <label htmlFor="user">User </label>
-            <select
-                name="user"
-                id="user"
-                onChange={(e) => { SetCurrentUser(e.target.value); }}
-            >
-                <option value={"Signed out"}>
-                    Signed out
-                </option>
-                    {users.map((user) => {
-                        
-                        return <option key={user.id} value={user.id}>{user.properties.Name.title[0].plain_text}</option>
-                        
-                   
-                })}
-            </select>
-        </form>
-    </div>)
-}
 
 function ManageHome({ results, CurrentUser }){ // Huvudsidan
     return (
