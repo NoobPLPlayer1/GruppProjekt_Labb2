5dc674f2abcf44c022e010c85f0605734fe0dede
zinar daoud <zinar.daoud@edu.edugrade.se>
2022-03-26 15:28:57 +0100

    Manager table

--- pages/components/Timereport.js
+++ pages/components/Timereport.js
@@ -1,7 +1,6 @@
 import { useState } from "react";
 function Timereport(props) {
 
-    console.log(props);
     const projectNameDisplay = () => { //save in a list different options which contains name and id of each project
         
         let projectnames = [];
--- pages/manager.js
+++ pages/manager.js
@@ -1,35 +1,107 @@
-import format from "date-fns/format";
-import getDay from "date-fns/getDay";
-import parse from "date-fns/parse";
-import startOfWeek from "date-fns/startOfWeek";
-import React, { useState } from "react";
-import { Calendar, dateFnsLocalizer } from "react-big-calendar";
-import "react-big-calendar/lib/css/react-big-calendar.css";
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
+import React, { useState, useEffect } from "react";
+import { QueryDatabase , timereportId, peopleId , projectsId} from "./notion";
 
-const locales = {
-  "sv-SE": require("date-fns/locale/sv")
-}
-const localizer = dateFnsLocalizer({
-  format,
-  parse, 
-  startOfWeek, 
-  getDay, 
-  locales
-})
 
-const events = [
+function Manager() {
 
-]
-
-const Manager = () => {
+  const [timereports, setTimereports] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [events, setEvents] = useState([]);
+  const [projects, setProjects] = useState([]);
+  const [startDate, setStartDate] = useState(new Date().toISOString().slice(0,10));
+  const [endDate, setEndDate] = useState(new Date().toISOString().slice(0,10));
+  useEffect(()=>{
+    const userNames = {};
+      users.forEach((p) => {
+        userNames[p.id] = p.properties.Name.title[0].plain_text;
+      })
+      const projectNames = {};
+      projects.forEach((p) => {
+        projectNames[p.id] = p.properties.Projectname.title[0].plain_text;
+      })
+      const list = [];
+      timereports.forEach((p) => {
+        const date = p.properties.Date.date.start;
+        if(startDate <= date && endDate >= date) {
+          list.push({
+            id: p.id,
+            name: userNames[p.properties.Person.relation[0].id],
+            projectname:  projectNames[p.properties.Project.relation[0].id],
+            date: p.properties.Date.date.start,
+            notes: p.properties.Note.title[0].plain_text,
+            hours: p.properties.Hours.number,
+          })
+        }
+      });
+      setEvents(list);
+  },[timereports, users, startDate, endDate]);
+  useEffect(() => {
+    async function GetData() {
+      await Promise.all([
+        QueryDatabase(timereportId, undefined, undefined, setTimereports), 
+        QueryDatabase(peopleId, undefined, undefined, setUsers),
+        QueryDatabase(projectsId, undefined, undefined, setProjects)
+      ]);
+    }
+    GetData();
+    }, []);
   return (
       <div>
-          <Calendar localizer={localizer} events = {events} 
-          startAccessor = "start" endAccessor="end" style={{height: 500, margin: "50px"}}
+        <form>
+          <input
+            type="date"
+            id="start_datum"
+            name="start_datum"
+            
+            value={startDate} 
+            onChange={(e) => setStartDate(e.target.value)}
+            required
           />
-          
+          <input
+            type="date"
+            id="slut_datum"
+            name="slut_datum"
+            
+            value={endDate} 
+            onChange={(e) => setEndDate(e.target.value)}
+            required
+          />
+        </form>
+        <table>
+          <thead>
+            <tr>
+              <th>
+                Name
+              </th>
+              <th>
+                Projects
+              </th>
+              <th>
+                Hours
+              </th>
+              <th>
+                Note
+              </th>
+              <th>
+                Date
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              events.map((item) => {
+                return (
+                <tr key={item.id}>
+                  {<td>{item.name}</td>}
+                  {<td>{item.projectname}</td>}
+                  {<td>{item.hours}</td>}
+                  {<td>{item.notes}</td>}
+                  {<td>{item.date}</td>}
+                </tr>)
+              })
+            }
+          </tbody>
+        </table>
       </div>
   );
 }
